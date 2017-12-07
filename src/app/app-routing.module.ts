3b61cf179a63f1b7e80885c206a251a2c8719b8c
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2017-12-07 16:08:42 -0500

    Routing with params setup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CityComponent } from './city/city.component';
+
+const routes: Routes = [
+  { path:'', pathMatch:'full', redirectTo:'dc' },
+  { path:':city', component: CityComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
